f50f6d92acdeebe79011b5c518fed1b911549532
ZekaiTombul <zekaitombul@gmail.com>
2025-03-17 23:24:43 +0300

    Solved

--- src/main/java/org/example/rpg/Poisonable.java
+++ src/main/java/org/example/rpg/Poisonable.java
@@ -1,9 +1,11 @@
 package org.example.rpg;
 
+import org.example.rpg.utils.Constant;
+
 public interface Poisonable {
 
     default double poison(){
-
+        return Constant.POISON_PERCENTAGE;
     }
 
 }
